b5bb949b19a743f7f9630edc16233eeb0cec5d70
3ka1tz <ekaitzlopez2025@gmail.com>
2026-02-19 11:29:22 +0100

    Update

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -5,14 +5,14 @@ int main(int argc, char** argv)
 {
     if (argc > 2)
     {
-        std::cerr << "Usage: " << argv[0] << " [configuration file]\n"
-                  << "If no file is provided, ../conf/default.conf is used.\n";
+        std::cerr << "Usage 1: " << argv[0] << '\n';
+        std::cerr << "Usage 2: " << argv[0] << " [configuration file]\n";
         return 1;
     }
 
-    std::string conf = argc == 1 ? "../conf/default.conf" : argv[1];
+    std::string conf = (argc == 1) ? "../conf/default.conf" : argv[1];
+    std::cout << "Using " << conf << " as configuration file.\n";
+
     if (!isConfValid(conf))
         return 1;
-
-    
 }
