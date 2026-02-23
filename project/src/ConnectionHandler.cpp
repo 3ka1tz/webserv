c35797bb6862133802122bc08a89afcb3c5f0d6a
3ka1tz <ekaitzlopez2025@gmail.com>
2026-02-23 13:10:57 +0100

    Update

--- project/src/ConnectionHandler.cpp
+++ project/src/ConnectionHandler.cpp
@@ -3,25 +3,11 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "../include/RequestLine.hpp"
-
-/*
-void ConnectionHandler::handle(int client_fd)
-{
-    std::string response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Length: 13\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "Hello, World!";
-
-    send(client_fd, response.c_str(), response.size(), 0);
-
-    close(client_fd);
-}
-*/
 
 void ConnectionHandler::handle(int client_fd)
 {
@@ -44,18 +30,33 @@ void ConnectionHandler::handle(int client_fd)
 
     std::string path = req.uri;
     if (path == "/")
-        path = "/index.html";
+        path = "./www/index.html";
 
-    std::cout << "Requested path: " << path << std::endl;
+    std::ifstream file(path.c_str());
+    bool notFound = !file.is_open();
 
-    std::string response =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Length: 2\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "OK";
+    if (notFound)
+    {
+        file.close();
+        file.open("./www/error_pages/404.html");
+    }
 
-    send(client_fd, response.c_str(), response.size(), 0);
+    std::string body((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    std::ostringstream response;
+    if (notFound)
+        response << "HTTP/1.1 404 Not Found\r\n";
+    else
+        response << "HTTP/1.1 200 OK\r\n";
+
+    response << "Content-Length: " << body.size() << "\r\n"
+                << "Content-Type: text/html\r\n"
+                << "\r\n"
+                << body;
+
+    std::string respStr = response.str();
+
+    send(client_fd, respStr.c_str(), respStr.size(), 0);
 
     close(client_fd);
 }
