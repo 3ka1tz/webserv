e937f560e2624ed90ea418e58f9cebb3f109e5ad
3ka1tz <ekaitzlopez2025@gmail.com>
2026-04-21 13:40:02 +0200

    Small modification

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,10 +1,10 @@
 #ifndef UTILS_HPP
 # define UTILS_HPP
 
-# include <string>
+# include <string> // string
 
 std::string getExtension(const std::string& path);
 std::string getFilename(const std::string& path);
 std::string fileToString(const std::string& path);
 
-#endif
+#endif // UTILS_HPP
